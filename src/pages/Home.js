32a21cd06daeebc841c0288c067710ce059c2311
Rendi <rendicrew96@gmail.com>
2023-08-23 20:30:08 +0700

    fix: fix bug

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,93 +1,100 @@
-import { Layout } from "@/components/Layout";
+import { Layout } from "../components/Layout";
 import axios from "axios";
-import { useRouter } from "next/router";
 import * as React from "react"
 import toast from "react-hot-toast";
-import useSWR from "swr";
-import styles from './Home.module.css';
-import { DEFAULT_TOAST_MESSAGE } from "@/constant/toast";
+import styles from './Home.css';
+import { DEFAULT_TOAST_MESSAGE } from "../constant/toast";
+import { useNavigate } from "react-router";
 
-export default function Home(props: any) {
-  const router = useRouter();
-  const fetcher = (url: string) => fetch(url).then((res) => res.json());
-  const { data, error } = useSWR('/api/static', fetcher);
-  
-  if (!data) return <div>Loading...</div>;
+export default function Home(props) {
+  const [data, setData] = React.useState()
 
-  function handleOnclick(params: number) {
-    router.push(`/rule/${params}`);
+  const navigate = useNavigate()
+
+  React.useState(() => {
+    getData()
+  }, [])
+
+
+  async function getData() {
+    const res = await axios.get(`${process.env.REACT_APP_URL}/fetchRules`)
+    console.log(res.data.details);
+    setData(res.data.details)
+  }
+  function handleOnclick(params) {
+    navigate(`/rule/${params}`);
   }
 
-  function handleOndelete(params: number) {
+  function handleOndelete(params) {
     toast.promise(
-      axios.delete(`/api/rule/${params}`)
-      .then((res)=>{
-        setTimeout(() => {
-          router.reload()
-        }, 1000);
-      }),
+      axios.delete(`${process.env.REACT_APP_URL}/deleteRuleSet?ruleSetName=${params}`)
+        .then((res) => {
+          setTimeout(() => {
+            window.location.reload()
+          }, 1000);
+        }),
       {
-         ...DEFAULT_TOAST_MESSAGE,
-       success: 'Rule Successfully Deleted',
+        ...DEFAULT_TOAST_MESSAGE,
+        success: 'Rule Successfully Deleted',
       }
     );
   }
 
-  function handleReset(){
-    toast.promise(
-      axios.get(`/api/reset`)
-      .then((res)=>{
-        setTimeout(() => {
-          router.reload()
-        }, 1000);
-      }),
-      {
-         ...DEFAULT_TOAST_MESSAGE,
-       success: 'Rule Successfully Reset',
-      }
-    );
+  function handleOnedit(params){
+    navigate(`/editruleset/${params}`)
   }
+
+
 
   return (
     <Layout>
       <section className={styles.layout}>
-        <button onClick={() => router.push('/addrule')}>
-          tambah data
-        </button>
-        <button onClick={handleReset}>
-          reset data
-        </button>
-        <ul className={`space-y-5 ${styles.myCustomList}`}>
-          {data.db.map((data: any, index: any) => {
-            return (
-              <li key={index} className={`border shadow-md p-4 rounded-md flex justify-between ${styles.listItem}`}>
-                <div>
-                  <p>
-                    {data.name}
-                  </p>
-                  <p>Endpoint: {`/ruleengine/?rule=${data.endpoint}`}</p>
-                  <p>Body</p>
-                  {data.body.map((data: any, index: any) => {
+        {
+          data ?
+            <>
+              <button onClick={() => navigate('/addrule')}>
+                tambah data
+              </button>
+              <ul className={`space-y-5 ${styles.myCustomList}`}>
+                {data?.map((data, index) => {
+                  return (
+                    <li key={index} className={`border shadow-md p-4 rounded-md flex justify-between ${styles.listItem}`}>
+                      <div>
+                        <p>
+                          {data.name}
+                        </p>
+                        <p>Endpoint: {`/ruleengine/?rule=${data.endpoint}`}</p>
+                        <p>Body</p>
+                        {/* {data.body.map((data, index) => {
                     return (
                       <div key={index} className={`flex ${styles.dataItem}`}>
                         <p>{data.name} : </p>
                         <p>{data.type}</p>
                       </div>
                     )
-                  })}
-                </div>
-                <div className={`flex space-x-10 w-full ${styles.buttonGroup}`}>
-                  <button onClick={() => handleOnclick(data.id)} className={`${styles.detail} ${styles.button}`}>
-                    detail
-                  </button>
-                  <button onClick={() => handleOndelete(data.id)} className={`${styles.delete} ${styles.button}`}>
-                    delete
-                  </button>
-                </div>
-              </li>
-            )
-          })}
-        </ul>
+                  })} */}
+                      </div>
+                      <div className={`flex space-x-10 w-full ${styles.buttonGroup}`}>
+                        <button onClick={() => handleOnclick(data.endpoint)} className={`${styles.detail} ${styles.button}`}>
+                          detail
+                        </button>
+                        <button onClick={() => handleOnedit(data.endpoint)} className={`${styles.delete} ${styles.button}`}>
+                          edit
+                        </button>
+                        <button onClick={() => handleOndelete(data.endpoint)} className={`${styles.delete} ${styles.button}`}>
+                          delete
+                        </button>
+                      </div>
+                    </li>
+                  )
+                })}
+              </ul>
+            </>
+            :
+            <p>loading</p>
+        }
+
+
       </section>
     </Layout>
   )
